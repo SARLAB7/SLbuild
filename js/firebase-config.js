4d8546cdbdb8e5ec94a07b759982c39bf4355b80
SAR <76199245+SARLAB7@users.noreply.github.com>
2026-05-12 15:55:37 -0500

    Update Firebase config to use VITE environment variables

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,17 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-// Aquí irán las credenciales de Firebase de SARLAB
 const firebaseConfig = {
-  apiKey: import.meta.env.FB_API_KEY,
-  authDomain: import.meta.env.FB_AUTH_DOMAIN,
-  projectId: import.meta.env.FB_PROJECT_ID,
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
   storageBucket: "slbuild2.firebasestorage.app",
   messagingSenderId: "676234118482",
-  appId: import.meta.env.FB_APP_ID
+  appId: import.meta.env.VITE_FB_APP_ID
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
